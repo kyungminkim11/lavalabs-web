029152b55bd261542e31b172d639474c74563aa6
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-07-14 16:19:04 +0900

    Track the latest edited panorama image source

--- assets/360-editor-view-hook.js
+++ assets/360-editor-view-hook.js
@@ -1,16 +1,34 @@
 (() => {
   'use strict';
+
+  const originalCreateObjectURL = URL.createObjectURL.bind(URL);
+  if (!URL.createObjectURL.__lavaPatched) {
+    const wrappedCreateObjectURL = blob => {
+      const url = originalCreateObjectURL(blob);
+      if (blob instanceof Blob && blob.type.startsWith('image/')) {
+        window.__lava360LatestImageUrl = url;
+        window.__lava360ViewerConfig = Object.assign({}, window.__lava360ViewerConfig || {}, { panorama: url });
+        window.dispatchEvent(new CustomEvent('lava360:imagesource', { detail: { url, type: blob.type, size: blob.size } }));
+      }
+      return url;
+    };
+    wrappedCreateObjectURL.__lavaPatched = true;
+    wrappedCreateObjectURL.__lavaOriginal = originalCreateObjectURL;
+    URL.createObjectURL = wrappedCreateObjectURL;
+  }
+
   const pannellum = window.pannellum;
   if (!pannellum || typeof pannellum.viewer !== 'function' || pannellum.viewer.__lavaPatched) return;
   const originalViewer = pannellum.viewer;
-  const wrapped = function wrappedViewer(container, config) {
+  const wrappedViewer = function wrappedViewer(container, config) {
     const viewer = originalViewer.call(this, container, config);
     window.__lava360Viewer = viewer;
     window.__lava360ViewerConfig = config || {};
+    if (config?.panorama) window.__lava360LatestImageUrl = config.panorama;
     window.dispatchEvent(new CustomEvent('lava360:viewerready', { detail: { viewer, config: config || {} } }));
     return viewer;
   };
-  wrapped.__lavaPatched = true;
-  wrapped.__lavaOriginal = originalViewer;
-  pannellum.viewer = wrapped;
+  wrappedViewer.__lavaPatched = true;
+  wrappedViewer.__lavaOriginal = originalViewer;
+  pannellum.viewer = wrappedViewer;
 })();
