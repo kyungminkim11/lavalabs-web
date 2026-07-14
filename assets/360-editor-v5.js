4af56b889faa6b5d0422a15db411b0f012cdf245
kyungminkim11 <66110823+kyungminkim11@users.noreply.github.com>
2026-07-14 23:03:32 +0900

    Load freeform mosaic editor

--- assets/360-editor-v5.js
+++ assets/360-editor-v5.js
@@ -21,6 +21,7 @@
       await loadScript('assets/360-editor-view-hook.js?v=20260714-view-export4', true);
       await loadScript('assets/360-editor-pro-loader.js?v=20260713-pro1');
       await loadScript('assets/360-editor-view-export.js?v=20260714-view-export1');
+      await loadScript('assets/360-editor-free-mosaic.js?v=20260714-free-mosaic1');
       await loadScript('assets/360-editor-gpano-fix.js?v=20260713-naver360-1', true);
     } catch (error) {
       console.error(error);
